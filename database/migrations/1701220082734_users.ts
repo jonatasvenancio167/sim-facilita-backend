d892b1aebb28c2d22ddd1356fc5fb140e53ad4b2
jonatas venancio <jvenancio167@hotmail.com>
2023-11-29 20:45:26 -0300

    create migrations

---
+++ database/migrations/1701220082734_users.ts
@@ -0,0 +1,27 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('name')
+      table.string('last_name')
+      table.date('birthday')
+      table.string('phone')
+      table.string('mail')
+      table.string('password')
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
---
+++ database/migrations/1701221215592_commentaries.ts
@@ -0,0 +1,25 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'commentaries'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('username')
+      table.string('text_commentary')
+      table.integer('user_id').unsigned().references('users.id')
+      table.integer('post_id').unsigned().references('posts.id')
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
