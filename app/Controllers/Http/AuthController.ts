fbff08f1858364e5f26742fe370741193de2e29b
jonatas venancio <jvenancio167@hotmail.com>
2023-12-04 20:00:50 -0300

    add new fields in authcontroller

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,9 +1,19 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
+import Hash from '@ioc:Adonis/Core/Hash'
 
 export default class AuthController {
   public async register({ request, auth }: HttpContextContract ) {
-    const userData = request.only(['username', 'email', 'password'])
+    const userData = request.only([
+      'username', 
+      'email', 
+      'password', 
+      'username', 
+      'lastName', 
+      'birthday', 
+      'phone' 
+    ])
+
     const user = await User.create(userData)
 
     await auth.login(user)
@@ -11,11 +21,27 @@ export default class AuthController {
     return user
   }
 
-  public async login({ request, auth }: HttpContextContract) {
-    const { email, password } = request.only(['email', 'password'])
-    await auth.attempt(email, password)
+  public async login({ request, response, auth }: HttpContextContract) {
+    const email = request.input('email')
+    const password = request.input('password')
 
-    return auth.user
+    try {
+      const user = await User.query()
+                            .where('email', email)
+                            .first()
+    if (!user) {
+      throw new Error('Usuário não encontrado')
+    }
+
+    if (!(await Hash.verify(user.password, password))) {
+      throw new Error('Email ou senha invalidos')
+    }
+
+    const token = await auth.use('api').generate(user, { expiresIn: '24h' })
+      return {token, user}
+    } catch(e){
+      return response.unauthorized(e)
+    }
   }
 
   public async logout({ auth, response }: HttpContextContract) {
